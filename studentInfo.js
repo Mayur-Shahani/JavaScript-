0a41bacc5adde0b33ef8415cc799a8da93a92183
Mayur-Shahani <shahanimkwork@gmail.com>
2025-07-06 11:51:25 +0500

    Add student introduction program using JavaScript variables

---
+++ studentInfo.js
@@ -0,0 +1,13 @@
+let studentName = "Mayur Shahani"
+let age = 20
+let grade = "A"
+let schoolName = "SHU"
+let favouriteSubject = "Programming"
+
+console.log("My Name is " + studentName + ". I am " + age + " years old.");
+console.log("I study in class 12 at " + schoolName + ".");
+console.log("My Favourite Subject is " + favouriteSubject + ".");
+
+
+
+

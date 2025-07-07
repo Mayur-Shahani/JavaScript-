5baf9c80b6b32343e46848cd27cf59259a627412
Mayur-Shahani <shahanimkwork@gmail.com>
2025-07-07 15:25:53 +0500

    Showing stack vs heap memory behavior in JavaScript

---
+++ memory-behavior.js
@@ -0,0 +1,21 @@
+let x = 7
+let y = x
+y = 70
+
+console.log(x);  // Value cant change due to Stack Memory
+
+let School = {
+    name: "Mayur",
+    age: 20
+}
+
+let school02 = School
+school02.name = "Shahani"
+
+console.log(School.name);
+
+
+
+
+
+

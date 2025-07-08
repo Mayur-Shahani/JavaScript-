752d11c4338a7dfbfeae1ba280dfeadd587de45d
Mayur-Shahani <shahanimkwork@gmail.com>
2025-07-08 14:50:10 +0500

    feat: Add string concatenation and method examples in JavaScript
    - Used '+' operator and template literals to concatenate string and number
    - Demonstrated common string methods:
      - .length
      - .toUpperCase()
      - .toLowerCase()
      - .charAt(index)
      - .indexOf(char)
    - Output helps in understanding how strings are manipulated in JS

---
+++ string-concatenation-and-methods.js
@@ -0,0 +1,27 @@
+console.log("First Method to concatenate String and number");
+
+let name_01 = "Mayur Shahani"
+let age_01 = 20
+
+console.log(name_01 + age_01);
+
+console.log("Second Method to concatenate String and number");
+
+let name_02 = "Mayur Shahani"
+let age_02 = 20
+
+console.log(`My Name is ${name_02} and I am ${age_02} years old `);
+
+console.log("Some Methods Of Strings:");
+
+let name_03 = new String("Mayur")
+console.log(name_03.length);
+
+console.log(name_03.toUpperCase());
+
+console.log(name_03.toLowerCase());
+
+console.log(name_03.charAt(2));
+
+console.log(name_03.indexOf("y"));
+

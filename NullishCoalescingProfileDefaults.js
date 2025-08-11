ce8b7362424133b06b2485b2410d17b392a11dae
Mayur-Shahani <shahanimkwork@gmail.com>
2025-08-11 18:49:03 +0500

    Add example using nullish coalescing operator to set default profile values

---
+++ NullishCoalescingProfileDefaults.js
@@ -0,0 +1,10 @@
+const apiData = {
+    username: null ?? "Mayur",
+    bio: undefined ?? "Not Available",
+    followers: 0,
+    posts: null ?? [3],
+    theme: "dark",
+    
+    
+};
+console.log(`Username is ${apiData.username} and bio is ${apiData.bio}. The Followers is ${apiData.followers}. Total Posts is ${apiData.posts} and theme is ${apiData.theme}.`);
